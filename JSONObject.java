8015554e848f11d62d2b30e8f9b4704e2de0235a
Vansh Gour <vanshgour52@gmail.com>
2025-06-07 19:46:32 +0530

    Add files via upload

---
+++ JSONObject.java
@@ -0,0 +1,14 @@
+
+public class JSONObject {
+
+    public String getString(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getString'");
+    }
+
+    public int getInt(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInt'");
+    }
+
+}
